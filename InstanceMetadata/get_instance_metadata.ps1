8a23a0104f4354cf276caf51471d668b06e345d3
mayurivikrant <90373127+mayurivikrant@users.noreply.github.com>
2023-02-01 12:09:01 +0530

    Update and rename azure_vm_instance_metadata.ps1 to get_instance_metadata.ps1

---
+++ InstanceMetadata/get_instance_metadata.ps1
@@ -0,0 +1,5 @@
+param (
+ $scriptMetadataParam
+)
+
+Invoke-RestMethod -Headers @{"Metadata"="true"} -Method GET -NoProxy -Uri "http://169.254.169.254/metadata/instance/$scriptMetadataParam$?api-version=2021-02-01" | ConvertTo-Json 

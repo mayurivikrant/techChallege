fcd159cdd2e9d482302d07a7be1851b6a8f34399
mayurivikrant <90373127+mayurivikrant@users.noreply.github.com>
2023-02-04 20:37:14 +0530

    Update get_value_from_object_key.ps1

--- getValueFromObject/get_value_from_object_key.ps1
+++ getValueFromObject/get_value_from_object_key.ps1
@@ -1,1 +1,36 @@
-readme
+function getKeyValue ($inputObject,$key)
+{
+   #Get input object
+   $functionInput = $inputObject
+   $functionInput
+   #Calculate key lenght
+   $keyLenght = $key.Split('/').Length
+   $keyLenght
+   #Get keys
+   $keyValues = $key.Split('/')
+   $keyValues
+   #Convert to PSobject
+   $functionInputkey = $functionInput | ConvertFrom-Json
+
+   foreach ($key in $keyValues){
+
+    $functionInputkey = $functionInputkey | Select -ExpandProperty $key
+
+   }
+
+   Write-Host "key value is $functionInputkey"
+
+}
+
+#Main
+
+$input = @'
+{"a":{"b":{"c":"d",
+           "e":"f" }}}
+'@
+
+$key = 'a/b/c'
+
+getKeyValue $input $key
+
+
